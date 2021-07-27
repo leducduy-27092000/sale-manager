ebe2a9a6daeb387849bb3ae931b42a700203891d
duy27092000 <leducduy27092000@gmail.com>
2021-07-28 01:07:46 +0700

    fix four

--- src/main/java/com/stackjava/controller/MainController.java
+++ src/main/java/com/stackjava/controller/MainController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.stackjava.dao.OrderDAO;
 import com.stackjava.dao.ProductDAO;
@@ -44,6 +45,7 @@ public class MainController {
     }
  
     @RequestMapping("/")
+    @ResponseBody
     public String home() {
         return "index";
     }
